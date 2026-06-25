fb3ffae8979222c282e51f6a31f4104ca0e36325
Rutva Prajapati <rutvaprajapati66@gmail.com>
2026-06-25 12:30:51 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (81.77%) - LeetSync

---
+++ 792-binary-search/binary-search.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Function to perform binary search
+int search(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid; // Target found
+        } else if (nums[mid] < target) {
+            left = mid + 1; // Search right half
+        } else {
+            right = mid - 1; // Search left half
+        }
+    }  
+
+    return -1; // Target not found
+}
